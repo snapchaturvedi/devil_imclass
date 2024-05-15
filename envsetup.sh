7a7117eabc0b53db2588ed2d7df499d5631894f3
snapchaturvedi <37495099+snapchaturvedi@users.noreply.github.com>
2024-05-15 20:29:06 +1000

    Specified Python version (3.9.2) for file navigation

--- envsetup.sh
+++ envsetup.sh
@@ -1,8 +1,8 @@
 sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
-wget https://www.python.org/ftp/python/VERSION/Python-VERSION.tgz`
-sudo tar zxf Python-3.9.tgz
-rm -r Python-3.9.tgz
-cd Python-3.9
+wget https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tgz`
+sudo tar zxf Python-3.9.2.tgz
+rm -r Python-3.9.2.tgz
+cd Python-3.9.2
 sudo ./configure --enable-optimizations
 sudo make -j 4
 sudo make altinstall
