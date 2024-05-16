d4f5e0f93421985a2d7689d3f185a6735029d785
snapchaturvedi <37495099+snapchaturvedi@users.noreply.github.com>
2024-05-16 14:23:08 +1000

    Update libsetup.sh

--- libsetup.sh
+++ libsetup.sh
@@ -5,5 +5,8 @@ sudo apt install python3-pip --yes
 sudo apt install python3-gpiozero
 sudo apt install python3-RPi.GPIO
 sudo apt install python3-picamera
+sudo apt install python3-numpy
 python3 -m pip install tflite-runtime
+
+# To resolve numpy conflict issue with tflite-runtime
 sudo apt-get install libopenblas-dev --yes
