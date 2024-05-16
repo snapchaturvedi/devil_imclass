c52ff9ad9a26178c6bf036df194d833e87d5c6d8
snapchaturvedi <37495099+snapchaturvedi@users.noreply.github.com>
2024-05-16 12:40:21 +1000

    Create libsetup.sh

---
+++ libsetup.sh
@@ -0,0 +1,9 @@
+# Install pip3 (just if need be)
+sudo apt install python3-pip --yes
+
+# Libraries 
+sudo apt install python3-gpiozero
+sudo apt install python3-RPi.GPIO
+sudo apt install python3-picamera
+python3 -m pip install tflite-runtime
+sudo apt-get install libopenblas-dev --yes
